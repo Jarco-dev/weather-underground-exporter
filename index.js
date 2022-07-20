d16aaf4d4bfa5a22ff8dc1229d1db3adae4dcbdd
Jarco-dev <jarco@familiekers.nl>
2022-07-20 13:34:29 +0200

    Decrease minimal interval to 58s from 60s

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ class MetricsManager {
     start() {
         this.app.get("/metrics", async (req, res) => {
             try {
-                if (!this.lastRequestTime || Date.now() - this.lastRequestTime > 60000) {
+                if (!this.lastRequestTime || Date.now() - this.lastRequestTime > 58000) {
                     this.lastRequestTime = Date.now();
                     const res2 = await fetch(`${this.apiUrl}?numericPrecision=decimal&format=json&units=m&stationId=${this.stationId}&apiKey=${this.apiKey}`);
                     if (!res2.ok) console.warn("Unable to retrieve data from weather underground api", res);
